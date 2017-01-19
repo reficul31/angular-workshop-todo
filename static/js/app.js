bf9ffb74957cec790f40efd718ebd42f567c2207
reficul31 <reficul31@gmail.com>
2017-01-19 09:58:55 +0530

    Adding the remove todo and clear todo

--- static/js/app.js
+++ static/js/app.js
@@ -1,12 +1,28 @@
 angular.module('todoApp',[])
 .controller('todoCtrl', function($scope){
-	$scope.todoList = ['Code Like Crazy', 'Learn Angular', 'Become Rich']
-	$scope.todoAdd = function(w){
-		$scope.todoList.push($scope.newTodo);
+	$scope.todoList = [{text:'Code Like Crazy',done:false}, {text:'Learn Angular',done:false}, {text:'Become Rich',done:false}];
+	$scope.todoAdd = function(){
+		$scope.todoList.push({text:$scope.newTodo,done:false});
 		$scope.newTodo = "";
 
+	}
+	$scope.done = function(todo){
+		tempTodo = $scope.todoList[$scope.todoList.indexOf(todo)];
+		console.log(tempTodo.done);
+		if(tempTodo.done == false)
+			tempTodo.done = true;
+		else(tempTodo.done == true)
+			tempTodo.done = false;
+		$scope.todoList[$scope.todoList.indexOf(todo)]=tempTodo;
+		console.log(tempTodo);
+	}
+	$scope.removeDone= function(){
+		var tempList = $scope.todoList.filter(function(todo){
+			return !todo.done;
+		});
+		$scope.todoList = tempList;
 	};
-	$scope.remove= function(todo){
+	$scope.removeTodo = function(todo){
 		$scope.todoList.splice($scope.todoList.indexOf(todo),1);
-	};
+	}
 })
