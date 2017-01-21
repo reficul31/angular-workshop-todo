6993ea982b0d018477da13a6127c3d16e7d5fa5d
reficul31 <reficul31@gmail.com>
2017-01-21 13:27:51 +0530

    Adding the done counter

--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,14 @@
 angular.module('todoApp',[])
 .controller('todoCtrl', function($scope){
 	$scope.todoList = [{text:'Code Like Crazy',done:false}, {text:'Learn Angular',done:false}, {text:'Become Rich',done:false}];
+	$scope.countDone = function(){
+		var count = 0;
+		for(i=0; i<$scope.todoList.length; i++){
+			if($scope.todoList[i].done == false)
+				count++;
+		}
+		return count;
+	}
 	$scope.todoAdd = function(){
 		$scope.todoList.push({text:$scope.newTodo,done:false});
 		$scope.newTodo = "";
